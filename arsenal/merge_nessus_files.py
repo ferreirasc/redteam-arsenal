edcbf71afef27a2b83a4ad10a3be04ee5b57bc7f
ferreirasc <leoferreirafx@gmail.com>
2025-12-04 11:59:27 -0500

    tmp

--- arsenal/merge_nessus_files.py
+++ arsenal/merge_nessus_files.py
@@ -1,36 +1,46 @@
-# file: merger.py
-# based off: http://cmikavac.net/2011/07/09/merging-multiple-nessus-scans-python-script/
-# by: mastahyeti
-
 import xml.etree.ElementTree as etree
 import shutil
 import os
 
-first = 1
+first = True
+mainTree = None
+report = None
+
 for fileName in os.listdir("."):
-   if ".nessus" in fileName:
-      print(":: Parsing", fileName)
-      if first:
-         mainTree = etree.parse(fileName)
-         report = mainTree.find('Report')
-         report.attrib['name'] = 'Merged Report'
-         first = 0
-      else:
-         tree = etree.parse(fileName)
-         for host in tree.findall('.//ReportHost'):
-            existing_host = report.find(".//ReportHost[@name='"+host.attrib['name']+"']")
-            if not existing_host:
-                print "adding host: " + host.attrib['name']
-                report.append(host)
-            else:
-                for item in host.findall('ReportItem'):
-                    if not existing_host.find("ReportItem[@port='"+ item.attrib['port'] +"'][@pluginID='"+ item.attrib['pluginID'] +"']"):
-                        print "adding finding: " + item.attrib['port'] + ":" + item.attrib['pluginID']
-                        existing_host.append(item)
-      print(":: => done.")    
-   
-if "nss_report" in os.listdir("."):
-   shutil.rmtree("nss_report")
-   
+    if fileName.endswith(".nessus"):
+        print(":: Parsing", fileName)
+
+        if first:
+            mainTree = etree.parse(fileName)
+            report = mainTree.find('Report')
+            report.attrib['name'] = 'Merged Report'
+            first = False
+        else:
+            tree = etree.parse(fileName)
+            for host in tree.findall('.//ReportHost'):
+                host_name = host.attrib.get('name')
+                existing_host = report.find(f".//ReportHost[@name='{host_name}']")
+
+                if existing_host is None:
+                    print(f"adding host: {host_name}")
+                    report.append(host)
+                else:
+                    for item in host.findall('ReportItem'):
+                        port = item.attrib.get('port')
+                        pluginID = item.attrib.get('pluginID')
+
+                        query = f"ReportItem[@port='{port}'][@pluginID='{pluginID}']"
+                        if existing_host.find(query) is None:
+                            print(f"adding finding: {port}:{pluginID}")
+                            existing_host.append(item)
+
+        print(":: => done.")
+
+# Output folder handling
+if os.path.exists("nss_report"):
+    shutil.rmtree("nss_report")
+
 os.mkdir("nss_report")
+
 mainTree.write("nss_report/report.nessus", encoding="utf-8", xml_declaration=True)
+
